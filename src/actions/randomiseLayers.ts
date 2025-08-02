2f9e0ce3b9d3ea3fdd990c9cae8a8cf019e993bd
Felix Wu <fwu@shipserv.com>
2025-08-02 17:17:59 +0100

    Take swing into account when calculating layer pitch

--- src/actions/randomiseLayers.ts
+++ src/actions/randomiseLayers.ts
@@ -1,6 +1,7 @@
-import { BPM, Layers, LoadedFiles } from '../lib/store'
+import { BPM, Layers, LoadedFiles, Swing } from '../lib/store'
 
 export const randomiseLayers = async () => {
+  const swing = Swing.ref()
   const savedLayers = [...Layers.ref()]
   Layers.set([])
   await new Promise(r => setTimeout(r, 200))
@@ -17,22 +18,24 @@ export const randomiseLayers = async () => {
 
   const bpm = BPM.ref()
   const stepSizeInSamples = ((60 / bpm) * 44100) / 4
+  const getSwingOffset = (index: number) => {
+    if (index % 2 === 1) return 0
+    return (swing / 100) * stepSizeInSamples
+  }
 
   for (const layer of savedLayers) {
     const file = loadedFiles.find(f => f.name === layer.filename)
     if (!file) continue
 
-    const sliceLengths = file.slices.map((s, i) => {
+    const pitchMults = file.slices.map((s, i) => {
       const nextSliceStart = file.slices[i + 1]?.start || file.samples[0].length
-      return nextSliceStart - s.start
-    })
-    const shortestSliceLength = Math.min(...sliceLengths)
+      const sliceLength = nextSliceStart - s.start
+      const swungStepSize =
+        stepSizeInSamples + getSwingOffset(s.stepNum) - getSwingOffset(s.stepNum + 1)
 
-    // Calculate pitch shift required to match shortest slice length with step size
-    // pitchMult = 1 / Math.pow(2, pitchShift / 12)
-    // So: pitchShift = 12 * log2(1 / pitchMult)
-    // Where pitchMult = stepSizeInSamples / shortestSliceLength
-    const pitchMult = stepSizeInSamples / shortestSliceLength
+      return swungStepSize / sliceLength
+    })
+    const pitchMult = Math.max(...pitchMults)
     const pitchShift = 12 * Math.log2(1 / pitchMult)
 
     layer.pitch = Math.ceil(pitchShift)
